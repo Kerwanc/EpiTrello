a0d36115f053c3e121c5c5d89bd94cdf1106110d
Kerwanc <kerwan.c@gmail.com>
2026-01-20 09:41:23 +0100

    fix(backend): enable cors to fix fe and be communication on different port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  
   const port = process.env.PORT ?? 3001;
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
