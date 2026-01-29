ca87b15192199197e0135c08046a8b8413ad3916
Kerwanc <kerwan.c@gmail.com>
2026-01-29 15:19:42 +0100

    feat(backend-crud): implemented controller for cards endpoints

--- backend/src/modules/cards/cards.module.ts
+++ backend/src/modules/cards/cards.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Card } from './entities/card.entity';
+import { List } from '../lists/entities/list.entity';
+import { CardsService } from './services/cards.service';
+import { CardsController } from './controllers/cards.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Card, List])],
+  controllers: [CardsController],
+  providers: [CardsService],
+  exports: [CardsService],
+})
 export class CardsModule {}
