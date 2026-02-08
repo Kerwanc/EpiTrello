386ccbac482704c56974ea5e9d808395460e8b01
Kerwanc <kerwan.c@gmail.com>
2026-02-08 20:18:31 +0100

    feat(backend-crud): implemented controller for lists endpoints

--- backend/src/modules/lists/lists.module.ts
+++ backend/src/modules/lists/lists.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { List } from './entities/list.entity';
+import { Board } from '../boards/entities/board.entity';
+import { ListsService } from './services/lists.service';
+import { ListsController } from './controllers/lists.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([List, Board])],
+  controllers: [ListsController],
+  providers: [ListsService],
+  exports: [ListsService],
+})
 export class ListsModule {}
