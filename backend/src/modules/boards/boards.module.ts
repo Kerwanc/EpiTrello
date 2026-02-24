259d1e848ef14f9d64ee6a3be5fdd917c25313f7
Kerwanc <kerwan.c@gmail.com>
2026-02-24 17:39:33 +0100

    feat(backend-db): implemented entity for te board-member join table

--- backend/src/modules/boards/boards.module.ts
+++ backend/src/modules/boards/boards.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './entities/board.entity';
+import { BoardMember } from './entities/board-member.entity';
 import { BoardsService } from './services/boards.service';
 import { BoardsController } from './controllers/boards.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board])],
+  imports: [TypeOrmModule.forFeature([Board, BoardMember])],
   controllers: [BoardsController],
   providers: [BoardsService],
   exports: [BoardsService],
