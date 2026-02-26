708e57e77671020ea41a7ca0e63298c4e0dfa4cf
Kerwanc <kerwan.c@gmail.com>
2026-02-26 13:11:55 +0100

    feat(backend-notify): implemented schemas and dtos for notification

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,6 +13,7 @@ import { BoardMember } from './modules/boards/entities/board-member.entity';
 import { List } from './modules/lists/entities/list.entity';
 import { Card } from './modules/cards/entities/card.entity';
 import { Comment } from './modules/cards/entities/comment.entity';
+import { Notification } from './modules/notifications/entities/notification.entity';
 
 @Module({
   imports: [
@@ -23,7 +24,7 @@ import { Comment } from './modules/cards/entities/comment.entity';
       username: process.env.DATABASE_USER || 'postgres',
       password: process.env.DATABASE_PASSWORD || 'postgres',
       database: process.env.DATABASE_NAME || 'epitrello',
-      entities: [User, Board, BoardMember, List, Card, Comment],
+      entities: [User, Board, BoardMember, List, Card, Comment, Notification],
       synchronize: true,
       logging: true,
     }),
